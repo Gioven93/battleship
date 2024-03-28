59ede97c0e82187445116b517ef52646efec86b7
giorgio ventrella <gioschy93@gmail.com>
2024-03-28 12:15:32 +0100

    basic skeleton

--- battleship-game/src/App.tsx
+++ battleship-game/src/App.tsx
@@ -2,11 +2,209 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Fragment } from 'react'
+import { Menu, Popover, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { MagnifyingGlassIcon } from '@heroicons/react/20/solid'
+
+const user = {
+  name: 'Tom Cook',
+  email: 'tom@example.com',
+  imageUrl:
+    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+}
+const navigation = [
+  { name: 'Github Code Repository', href: '#', current: false },
+]
+
+function classNames(...classes: any[]) {
+  return classes.filter(Boolean).join(' ')
+}
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <div className="min-h-full">
+        <Popover as="header" className="bg-indigo-600 pb-24">
+          {({ open }) => (
+            <>
+              <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+                <div className="relative flex items-center justify-center py-5 lg:justify-between">
+                  {/* Logo */}
+                  <div className="absolute left-0 flex-shrink-0 lg:static">
+                    <a href="#">
+                      <span className="sr-only">GIORGIO VENTRELLA</span>
+                      <h1 className='uppercase text-white font-semibold'>Giorgio Ventrella Battleship Game</h1>
+                    </a>
+                  </div>
+
+                  {/* Menu button */}
+                  <div className="absolute right-0 flex-shrink-0 lg:hidden">
+                    {/* Mobile menu button */}
+                    <Popover.Button className="relative inline-flex items-center justify-center rounded-md bg-transparent p-2 text-indigo-200 hover:bg-white hover:bg-opacity-10 hover:text-white focus:outline-none focus:ring-2 focus:ring-white">
+                      <span className="absolute -inset-0.5" />
+                      <span className="sr-only">Open main menu</span>
+                      {open ? (
+                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                      ) : (
+                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                      )}
+                    </Popover.Button>
+                  </div>
+                </div>
+                <div className="hidden border-t border-white border-opacity-20 py-5 lg:block">
+                  <div className="grid grid-cols-3 items-center gap-8">
+                    <div className="col-span-2">
+                      <nav className="flex space-x-4">
+                        {navigation.map((item) => (
+                          <a
+                            key={item.name}
+                            href={item.href}
+                            className={classNames(
+                              item.current ? 'text-white' : 'text-indigo-100',
+                              'rounded-md bg-white bg-opacity-0 px-3 py-2 text-sm font-medium hover:bg-opacity-10'
+                            )}
+                            aria-current={item.current ? 'page' : undefined}
+                          >
+                            {item.name}
+                          </a>
+                        ))}
+                      </nav>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <Transition.Root as={Fragment}>
+                <div className="lg:hidden">
+                  <Transition.Child
+                    as={Fragment}
+                    enter="duration-150 ease-out"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="duration-150 ease-in"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <Popover.Overlay className="fixed inset-0 z-20 bg-black bg-opacity-25" />
+                  </Transition.Child>
+
+                  <Transition.Child
+                    as={Fragment}
+                    enter="duration-150 ease-out"
+                    enterFrom="opacity-0 scale-95"
+                    enterTo="opacity-100 scale-100"
+                    leave="duration-150 ease-in"
+                    leaveFrom="opacity-100 scale-100"
+                    leaveTo="opacity-0 scale-95"
+                  >
+                    <Popover.Panel
+                      focus
+                      className="absolute inset-x-0 top-0 z-30 mx-auto w-full max-w-3xl origin-top transform p-2 transition"
+                    >
+                      <div className="divide-y divide-gray-200 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+                        <div className="pb-2 pt-3">
+                          <div className="flex items-center justify-between px-4">
+                            <div>
+                              <img
+                                className="h-8 w-auto"
+                                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                                alt="Your Company"
+                              />
+                            </div>
+                            <div className="-mr-2">
+                              <Popover.Button className="relative inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                <span className="absolute -inset-0.5" />
+                                <span className="sr-only">Close menu</span>
+                                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                              </Popover.Button>
+                            </div>
+                          </div>
+                          <div className="mt-3 space-y-1 px-2">
+                            <a
+                              href="#"
+                              className="block rounded-md px-3 py-2 text-base font-medium text-gray-900 hover:bg-gray-100 hover:text-gray-800"
+                            >
+                              Github Code Repository
+                            </a>
+                            
+                          </div>
+                        </div>
+                        <div className="pb-2 pt-4">
+                          <div className="flex items-center px-5">
+                            <div className="min-w-0 flex-1">
+                              <div className="truncate text-base font-medium text-gray-800">Giorgio Ventrella</div>
+                              <div className="truncate text-sm font-medium text-gray-500">giorgio.ventrella@gmail.com</div>
+                            </div>
+                            
+                          </div>
+                        </div>
+                      </div>
+                    </Popover.Panel>
+                  </Transition.Child>
+                </div>
+              </Transition.Root>
+            </>
+          )}
+        </Popover>
+        <main className="-mt-24 pb-8">
+          <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+            <h1 className="sr-only">Page title</h1>
+            {/* Main 3 column grid */}
+            <div className="grid grid-cols-1 items-start gap-4 lg:grid-cols-4 lg:gap-8">
+              {/* Left column */}
+              <div className="grid grid-cols-1 gap-4 lg:col-span-2">
+                <section aria-labelledby="section-1-title">
+                  <h2 className="sr-only" id="section-1-title">
+                    Section title
+                  </h2>
+                  <div className="overflow-hidden rounded-lg bg-white shadow">
+                    <div className="p-6">
+                      <p>CIAO</p>
+                      <p>CIAO</p>
+                      <p>CIAO</p>
+                      <p>CIAO</p>
+                      <p>CIAO</p>
+                      <p>CIAO</p>
+                      
+
+                    </div>
+                  </div>
+                </section>
+              </div>
+
+              {/* Right column */}
+              <div className="grid grid-cols-1 gap-4 lg:col-span-2">
+                <section aria-labelledby="section-2-title">
+                  <h2 className="sr-only" id="section-2-title">
+                    Section title
+                  </h2>
+                  <div className="overflow-hidden rounded-lg bg-white shadow">
+                    <div className="p-6"> <p>CIAO</p>
+                    
+                    <p>CIAO</p>
+                    <p>CIAO</p>
+                    <p>CIAO</p>
+                    <p>CIAO</p>
+                    <p>CIAO</p>
+                    <p>CIAO</p>
+                    <p>CIAO</p></div>
+                  </div>
+                </section>
+              </div>
+            </div>
+          </div>
+        </main>
+        <footer>
+          <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+            <div className="border-t border-gray-200 py-8 text-center text-sm text-gray-500 sm:text-left">
+              <span className="block sm:inline">&copy; 2021 Your Company, Inc.</span>{' '}
+              <span className="block sm:inline">All rights reserved.</span>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </>
   );
 }
 
